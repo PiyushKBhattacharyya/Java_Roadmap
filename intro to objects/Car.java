83a777061980450b9102665981414fd0c23d0644
Piyush <91675982+PiyushKBhattacharyya@users.noreply.github.com>
2025-05-16 11:49:12 +0530

    introduction to objects

---
+++ intro to objects/Car.java
@@ -0,0 +1,18 @@
+package intro;
+
+public class Car {
+    String color;
+    int year;
+    void drive() {
+        System.out.println("The car is driving.");
+    }
+
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.color = "Red";
+        myCar.year = 2022;
+        myCar.drive();
+        System.out.println("Color: " + myCar.color);
+        System.out.println("Year: " + myCar.year);
+    }
+} 
